9901730e0ce675a6a3ff1c855bdc54dc824b2cd2
Stephanos Theodotou <stephanostheodotou@Stephanoss-MBP.home>
2017-08-19 22:23:20 +0200

    fix auth issue

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -10,6 +10,40 @@ const User = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
 
+router.post('/login', (req, res, next) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  console.log(email, password);
+  if (!email || !password) {
+    res.status(401).json({ message: 'Provide email and password' });
+    return;
+  }
+
+  User.findOne({'email': email}, (err, user) => {
+    if (!user) {
+      res.status(401).json({ message: 'The email or password is incorrect' });
+      return;
+    }
+
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (!isMatch) {
+        res.status(401).json({ message: 'The email or password is incorrect' });
+      }
+      else {
+        const payload = {id: user._id, email: user.email};
+        const token = jwt.sign(payload, jwtOptions.secretOrKey);
+        res.status(200).json({ token, user });
+        console.log("this is the token, ", token);
+        console.log("and the user : ", user);
+      }
+    });
+  });
+});
+
+router.get('/dashboard', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json();
+});
+
 router.post('/signup', (req, res, next) => {
   let email = req.body.email;
   let password = req.body.password;
@@ -40,44 +74,11 @@ router.post('/signup', (req, res, next) => {
       else {
         const payload = {id: user._id, email: user.email};
         const token = jwt.sign(payload, jwtOptions.secretOrKey);
-
+        
         res.status(200).json({ token, user });
       }
     });
   });
-});
-
-router.post('/login', (req, res, next) => {
-  let email = req.body.email;
-  let password = req.body.password;
-
-  if (!email || !password) {
-    res.status(401).json({ message: 'Provide email and password' });
-    return;
-  }
-
-  User.findOne({'email': email}, (err, user) => {
-    if (!user) {
-      res.status(401).json({ message: 'The email or password is incorrect' });
-      return;
-    }
-
-    bcrypt.compare(password, user.password, (err, isMatch) => {
-      if (!isMatch) {
-        res.status(401).json({ message: 'The email or password is incorrect' });
-      }
-      else {
-        const payload = {id: user._id, email: user.email};
-        const token = jwt.sign(payload, jwtOptions.secretOrKey);
-
-        res.status(200).json({ token, user });
-      }
-    });
-  });
-});
-
-router.get('/private', passport.authenticate('jwt', { session: false }), (req, res) => {
-  res.json();
 });
 
 module.exports = router;
